c29f9afdb4684126812d62afacf3a6d1e2201b5c
Dima Mikhaylov <47697555+allaccountstaken@users.noreply.github.com>
2023-10-19 21:01:39 -0400

    Update perceptron.py
    Original perceptron

--- bin/perceptron.py
+++ bin/perceptron.py
@@ -1,25 +1,14 @@
-''' This is module docstring'''
 class Perceptron:
-    '''Class Perceptron'''
-
-    def __init__(self, inputs):
-        '''Initialization'''
-        self.dummied_inputs = inputs
-        self._weights = None
-
-    def train(self, inputs, labels):
-        '''Training on labeled data'''
-        dummied_inputs = [observation + [-1] for observation in inputs]
-        self._weights = [0.2] * len(dummied_inputs[0])
-        for _ in range(5000):
-            for value, label in zip(dummied_inputs, labels):
-                label_delta = label - self.predict(inputs)
-                self._weights[value] += .1 * value * label_delta
-
-
-    def predict(self, new_data):
-        '''Label new data'''
-        if len(new_data) == 0:
-            return None
-        new_data = new_data + [-1]
-        return int(0 < sum([x[0]*x[1] for x in zip(self._weights, new_data)])) # pylint: disable=R1728
+  def train(self, inputs, labels):
+    dummied_inputs = [x + [-1] for x in inputs]
+    self._weights = [0.2] * len(dummied_inputs[0])
+    for _ in range(5000):
+      for input, label in zip(dummied_inputs, labels):
+        label_delta = (label - self.predict(input))
+        for index, x in enumerate(input):
+          self._weights[index] += .1 * x * label_delta
+  def predict(self, input):
+    if len(input) == 0:
+      return None
+    input = input + [-1]
+    return int(0 < sum([x[0]*x[1] for x in zip(self._weights, input)])) # pylint: disable=R1728
