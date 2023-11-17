03fc4b09ae918c9f25d8cb38e3e514b9ff96122b
Dmitry Mikhaylov <dmik@vt.edu>
2023-11-17 13:02:37 -0500

    passing pylint after fixing test_perceptron

--- bin/perceptron.py
+++ bin/perceptron.py
@@ -1,14 +1,35 @@
+
+"""
+    This module is used to create, train and predict using perceptron
+    """
+
 class Perceptron:
-  def train(self, inputs, labels):
-    dummied_inputs = [x + [-1] for x in inputs]
-    self._weights = [0.2] * len(dummied_inputs[0])
-    for _ in range(5000):
-      for input, label in zip(dummied_inputs, labels):
-        label_delta = (label - self.predict(input))
-        for index, x in enumerate(input):
-          self._weights[index] += .1 * x * label_delta
-  def predict(self, input):
-    if len(input) == 0:
-      return None
-    input = input + [-1]
-    return int(0 < sum([x[0]*x[1] for x in zip(self._weights, input)])) # pylint: disable=R1728
+    """
+        This class represent a small model of perceptron
+        """
+    def __init__(self):
+        """
+            Create empty perceptron, set original weight to None
+            """
+        self._weights = None
+
+    def train(self, inputs, labels):
+        """
+            Train the model by taking inputs and corresponding labels, adjust the weights
+            """
+        dummied_inputs = [x + [-1] for x in inputs]
+        self._weights = [0.2] * len(dummied_inputs[0])
+        for _ in range(5000):
+            for dummy_input, label in zip(dummied_inputs, labels):
+                label_delta = label - self.predict(dummy_input)
+                for index, item in enumerate(dummy_input):
+                    self._weights[index] += .1 * item * label_delta
+
+    def predict(self, inputs):
+        """
+            Make predictions by taking new inputs and adjusted (trained) weights
+            """
+        if len(inputs) == 0:
+            return None
+        inputs = inputs + [-1]
+        return int(0 < sum([x[0]*x[1] for x in zip(self._weights, inputs)])) #pylint: disable=R1728
--- tests/test_perceptron.py
+++ tests/test_perceptron.py
@@ -1,44 +1,75 @@
 import sys
 sys.path.append(".")
-
-#import bin.clockdeco_param as cp
+import pytest
+import os
+from sys import platform
 from bin.perceptron import Perceptron # TODO: import the perceptron directly
 
 def test_perceptron():
+    # Given properly initialized perceptron
     the_perceptron = Perceptron()
-    the_perceptron.train([
-        [1,1],
-        [1,0],
-        [0,1],
-        [0,0],
-    ], [1,1,1,0])
+    # When inputs and labels are provided
+    the_perceptron.train([[1,1],[1,0],[0,1],[0,0],], # inputs
+                         [1,1,1,0]) # labels
+    # Then labels should be produced in predictions
+    assert the_perceptron.predict([1,1]) ==  1, "test [1,1] result in 1"
+    assert the_perceptron.predict([1,0]) ==  1, "test [1,0] result in 1"
+    assert the_perceptron.predict([0,1]) ==  1, "test [0,1] result in 1"
+    assert the_perceptron.predict([0,0]) ==  0, "test [0,0] result in 0"
 
-    assert the_perceptron.predict([1,1]) ==  1, "assert comment"
-    assert the_perceptron.predict([1,0]) ==  1, "assert comment"
-    assert the_perceptron.predict([0,1]) ==  1, "assert comment"
-    assert the_perceptron.predict([0,0]) ==  0, "assert comment"
-
-# TODO: make them fail, use "f strings" for comments
-
-# TODO: negative test, create a new test, then purposely replace the four assertion with one failing assertion
-# This should show up 'red' on your test output for now, since it is of course, failing.
 # TODO: add the proper pytest decorator, so that it is obvious it is expected to fail, XFAIL
+@pytest.mark.xfail(reason="expected to fail", strict=False)
+def test_tofail():
+    # Given properly initialized perceptron
+    the_perceptron = Perceptron()
+    # When inputs and labels are provided
+    the_perceptron.train([[1,1],[1,0],[0,1],[0,0],], # inputs
+                         [1,1,1,0]) # true labels
+    # Then label 10 should not be produced in predictions
+    assert the_perceptron.predict([1,1]) ==  10, "failing assertion"
 
 
+# TODO: write a test that is conditionally skipped, skipif the OS is not linux ubuntu and vs
+@pytest.mark.skipif(platform != 'darwin', reason="this test for Mac OS only")
+def test_forMacOSonly():
+    print("This test should only run on Mac OS and the current platform is", platform)
 
-# TODO: write a test that is conditionally skipped, skipif the OS is not linux ubuntu (???)
 
+@pytest.mark.skipif(platform != 'Linux', reason="this test for Ubuntu only")
+def test_forUbuntuOSonly():
+    print("This test should only run on Ubuntu OS and the current platform is", platform)
+    # Given os.popen is available, run Linux bash command
+    total_memory, used_memory, free_memory = map(int, os.popen('free -t -m').readlines()[-1].split()[1:])
+    # Then assert memory usage
+    assert total_memory > 0, 'Linux total memory is greater than 0'
+    assert used_memory > 0, 'Linux used memory is greater than 0'
+    assert free_memory > 0, 'Linux free memory is greater than 0'
 
-
-
-
-# TODO: Write another test, put a bogus reason in your decorator like This test is not yet ready for prime time and add the decorator so the test is always skipped.
-
+# TODO: Write another test, put a bogus reason in your decorator like This test is not yet ready for 
+# prime time and add the decorator so the test is always skipped.
+@pytest.mark.skip(reason="Not ready for prime time")
+def test_primetime():
+    print("This test should be alsways skipped")
 
 
 # TODO: parametarization, say you wanted to run 3 or 5 or 10. The inputs should be "trainingset, labels, expected". Have at least 5 or 6 different datasets and make the test run. 
 
+@pytest.mark.parametrize("trainingset, labels, expected", #three type of things used in test
+                         [
+                          ([[1,1], [1,0], [0,1], [0,0]], [1,1,1,0], [1,1,1,0]), # this row is true data
+                          ([[0,0], [0,0], [0,0], [0,0]], [0,0,0,0], [0,0,0,0]), # default all 0 case
+                          ([[1,1], [1,1], [1,1], [1,1]], [1,1,1,1], [1,1,1,1]), # default all 1s case
+                          ([[1,1], [1,0], [0,1], [0,0]], [1,1,1,1], [1,1,1,1]), # wrong lables
+                          ([[1,1], [1,0], [1,1], [0,0]], [1,1,1,0], [1,1,1,0]) # different traing 
+                         ])
+def test_papametrized(trainingset, labels, expected):
+    # Given properly initialized perceptron
+    the_perceptron = Perceptron()
+    # When inputs and labels are provided
+    the_perceptron.train(trainingset, labels)
+    # Then check if predictions match expected
+    for n, true_labels in zip(trainingset, expected):
+        assert the_perceptron.predict(n) == true_labels, f"testing for row number {n}"
 
-
-
-# TODO fixtures. instead of training a Perceptron with the same data set for 3 tests, have the Perceptron trained once in a function, and use a fixture to provide that to each of the 3 tests.
+# TODO fixtures. instead of training a Perceptron with the same data set for 3 tests,
+#  have the Perceptron trained once in a function, and use a fixture to provide that to each of the 3 tests.
